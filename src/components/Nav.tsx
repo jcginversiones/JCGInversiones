cce3704370152bff62efb432c24c53f03630f27b
danielpergst <dpereyra@gstint.com.ar>
2024-09-12 17:08:14 -0300

    se optimiza el Nav para vista mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import ButtonAction from './ButtonAction';
 import './Nav.css';
-import { useState } from 'react';
 
 const Nav : React.FC = () =>{
-    const [activeSection, setActiveSection] = useState<string>('header');
 
 
     return(
@@ -11,9 +9,9 @@ const Nav : React.FC = () =>{
             <div className='navSupContainer'>
                 <h3><a href="#header">Juan Giacovino</a></h3>
                 <ul className='ulNS'>
-                    <li><a href="#presentation" className={activeSection === 'presentation' ? 'active' : ''}>Sobre Mi</a></li>
-                    <li><a href="#services" className={activeSection === 'services' ? 'active' : ''}>Servicios</a></li>
-                    <li><a href="#contact" className={activeSection === 'contact' ? 'active' : ''}>Contacto</a></li>
+                    <li><a href="#presentation">Sobre Mi</a></li>
+                    <li><a href="#services">Servicios</a></li>
+                    <li><a href="#contact">Contacto</a></li>
                 </ul>
                 <ButtonAction text='Descargas' colorSet={2} to='#services'/>
             </div>
