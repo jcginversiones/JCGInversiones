e3f5ae6f5ae921fcc9662648ce88eafd6e087313
oidmortvles <dpereyra@gstint.com.ar>
2025-04-06 22:41:37 -0300

    Se actualizan los datos de contacto en /Header, /Presentation y /Contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header : React.FC<HeaderProps> = ({ id }) =>{
                     <p className='hSText2'>Análisis detallados y herramientas profesionales para ayudarte a maximizar tus rendimientos</p>
 
                     <div className='hSButtons'>
-                        <ButtonAction  text='Pedir asesoria' colorSet={1} to='#contact' />
+                        <ButtonAction colorSet={1} text='Pedir asesoria' to='https://wa.me/541169787999?text=Hola%20JCG Inversiones,%20encontré%20tu%20perfil%20a%20través%20de%20la%20web%20y%20me%20interesaba%20saber%20sobre%20tus%20Servicios%20en%20Trading'/>
                         <ButtonAction  text='Conoce nuestros Servicios' colorSet={3} to='#services  ' />
                     </div>
                 </section>
