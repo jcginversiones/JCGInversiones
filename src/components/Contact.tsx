5c556b9040efca4056539ebf2e5b99796a9f0176
jcginversiones <juancgiacovino@gmail.com>
2026-05-04 08:47:16 -0300

    Add files via upload

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -21,8 +21,15 @@ const Contact : React.FC <ContactProp> = ({id}) =>{
                             En nuestra comunidad hay gente como vos, y entre todos construimos el futuro que buscamos
                         </p>
                         <div className='cButtons'>
-                            <ButtonAction colorSet={1} text='Enviame un Whatsapp' to='https://wa.me/541169787999?text=Hola%20JCG Inversiones,%20encontré%20tu%20perfil%20a%20través%20de%20la%20web%20y%20me%20interesaba%20saber%20sobre%20tus%20Servicios%20en%20Trading'/>
-                            <ButtonAction colorSet={3} text='Enviame un Mail' to='mailto:juancg00ginversiones@gmail.com?subject=Consulta%20desde%20Web' />                            
+                            <a 
+                                href="https://cal.com/jcgstrategic/jcginversiones" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className='btnAgendar'
+                            >
+                                📅 Agendar Reunión
+                            </a>
+                            <ButtonAction colorSet={1} text='Whatsapp' to='https://wa.me/541169787999?text=Hola%20JCG Inversiones,%20encontré%20tu%20perfil%20a%20través%20de%20la%20web%20y%20me%20interesaba%20saber%20sobre%20tus%20Servicios%20en%20Trading'/>
                         </div>
                 </article>                
                 <RedesContact/>
