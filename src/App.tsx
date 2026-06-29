3c5138e8c3e8713568b9522715e7b263719dc2a9
jcginversiones <juancgiacovino@gmail.com>
2026-06-28 22:47:27 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.css';
 import Header from './components/Header';
 import Presentation from './components/Presentation';
 import Services from './components/Services';
+import RealEstate from './components/RealEstate';
 import Contact from './components/Contact';
-function App() {
 
-  return (
-    <>
-      <Header id="header" />
-      <Presentation id="presentation" />
-      <Services id="services" />
-      <Contact id="contact" />
-    </>
-  )
+function App() {
+    return (
+        <>
+            <Header id="header" />
+            <Services id="services" />
+            <RealEstate />
+            <Presentation id="presentation" />
+            <Contact id="contact" />
+        </>
+    )
 }
 
-export default App
+export default App;
