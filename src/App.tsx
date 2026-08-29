ceef11a95060a4181fc45c82c126c0e9c965e79a
jcginversiones <juancgiacovino@gmail.com>
2026-08-29 18:00:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css';
 import Header from './components/Header';
-import Presentation from './components/Presentation';
-import Services from './components/Services';
+import PlanesServices from './components/PlanesServices';
+import EmpresasServices from './components/EmpresasServices';
 import RealEstate from './components/RealEstate';
+import Presentation from './components/Presentation';
+import SectionDowloads from './components/SectionDowloads';
 import Contact from './components/Contact';
 
 function App() {
     return (
         <>
             <Header id="header" />
-            <Services id="services" />
+            <PlanesServices id="services" />
+            <EmpresasServices id="empresas" />
             <RealEstate />
+            <SectionDowloads />
             <Presentation id="presentation" />
             <Contact id="contact" />
         </>
